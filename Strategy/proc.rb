6130cd0895e7d012da34bf6ac4ca56559ffddea0
JRubyist <unix1983@gmail.com>
2014-05-21 10:10:14 +0800

    proc

---
+++ Strategy/proc.rb
@@ -0,0 +1,13 @@
+def run_it
+  puts 'Before the yield'
+  yield
+  puts 'After the yield'
+end
+
+run_it { puts 'Hello' }
+
+is_proc = Proc.new{puts 'test call it.'}
+
+is_proc.call
+
+run_it {is_proc.call}

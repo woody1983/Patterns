1471783a78d6a5fe334456813bd940267f80f468
JRubyist <unix1983@gmail.com>
2014-05-22 01:54:18 +0800

    About Proc & lambda

--- Strategy/proc.rb
+++ Strategy/proc.rb
@@ -13,3 +13,25 @@ puts '>>>>>>>>>>>>>>>>>>>>>>>Other one>>>>>>>>>>>>>>>>>>>>>>>>'
 is_proc.call
 
 run_it {is_proc.call}
+puts '>>>>>>>>>>>>>>>>>>>>>>>take arg into proc>>>>>>>>>>>>>>>>>>>>>>>>'
+
+def run_it_with_parameter
+  puts 'Before the yield'
+  yield(24)
+  puts 'After the yield'
+end
+
+run_it_with_parameter do |x|
+  puts 'Hello from inside the proc'
+  puts "The value of x is #{x}"
+end
+def run_it_with_parameter_copy(&block)
+  puts 'Before the yield'
+  block.call(24)
+  puts 'After the yield'
+end
+
+run_it_with_parameter_copy {|x| puts "It's #{x}"}
+my_proc = lambda {|x| puts "The value is #{x}" }
+
+run_it_with_parameter(&my_proc)
